54b06139169217d64fdb056a0524496e2a4c2061
zhouqingkai <1208678644@qq.com>
2024-10-25 14:17:02 +0800

    fix:优化 Redis 分布式锁实现
    - 添加 UUID前缀以区分不同实例的锁
    - 修正解锁逻辑，增加线程 ID 检查以防止误删其他线程的锁
    - 优化代码结构，提高可读性和可维护性

--- hm-dianping/src/main/java/com/hmdp/redis/RedisLockImpl.java
+++ hm-dianping/src/main/java/com/hmdp/redis/RedisLockImpl.java
@@ -1,15 +1,17 @@
 package com.hmdp.redis;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
 
 public class RedisLockImpl implements RedisLock {
 
-    private final String name;  //业务名
-    private StringRedisTemplate stringRedisTemplate;
+    private final String name;  //锁的名字
+    private final StringRedisTemplate stringRedisTemplate;
 
-    private static final String KEYPREFIX = "lock:";
+    private static final String PREFIX = "lock:";
+    private static final String VALUE_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     public RedisLockImpl(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -18,16 +20,19 @@ public class RedisLockImpl implements RedisLock {
 
     @Override
     public boolean getLock(Long expireTime) {
-        long threadId = Thread.currentThread().getId();
-        Thread.currentThread().getId();
+        String value = VALUE_PREFIX + Thread.currentThread().getId();
         Boolean result = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEYPREFIX + name, threadId + "", expireTime, TimeUnit.SECONDS);
+                .setIfAbsent(PREFIX + name, value, expireTime, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(result);
     }
 
     @Override
     public void unLock() {
-        stringRedisTemplate.delete(KEYPREFIX + name);
-
+        //解决极限情况下,误删锁的情况。
+        String value = VALUE_PREFIX + Thread.currentThread().getId();
+        String currenThreadId = stringRedisTemplate.opsForValue().get(PREFIX + name);
+        if (value.equals(currenThreadId)) {
+            stringRedisTemplate.delete(PREFIX + name);
+        }
     }
 }
