0f723893db1e8c811e2ed4387d3f008931cb4649
zhouqingkai <1208678644@qq.com>
2024-11-01 11:09:27 +0800

    refactor(hm-dianping): 重构 Blog 相关接口和实现
    - 将 BlogController 中的方法移到 BlogServiceImpl 中
    - 新增 queryHotBlog、queryBlogById、queryMyBlog、likeBlog等接口和实现
    -优化 queryShopById 方法，解决调用方法错误的bug
    - 更新 IMAGE_UPLOAD_DIR 常量路径为nginx存储路径

--- hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,14 +1,23 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.constant.SystemConstants;
+import com.hmdp.constant.UserHolder;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author dadaguai
@@ -17,4 +26,70 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    private final IUserService userService;
+
+    public BlogServiceImpl(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = this.query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(this::queryUserByBlog);
+        return Result.ok(records);
+    }
+
+    @Override
+    public Result queryBlogById(Long id) {
+        Blog blog = this.getById(id);
+        if (blog==null){
+            return Result.fail("笔记不存在");
+        }
+        queryUserByBlog(blog);
+        return Result.ok(blog);
+    }
+
+    @Override
+    public Result queryMyBlog(Integer current) {
+        // 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        // 根据用户查询
+        Page<Blog> page = this.query()
+                .eq("user_id", user.getId()).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        return Result.ok(records);
+    }
+
+    @Override
+    public Result likeBlog(Long id) {
+        // 修改点赞数量
+        this.update()
+                .setSql("liked = liked + 1").eq("id", id).update();
+        return Result.ok();
+    }
+
+    @Override
+    public Result saveBlog(Blog blog) {
+        // 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        blog.setUserId(user.getId());
+        // 保存探店博文
+        this.save(blog);
+        // 返回id
+        return Result.ok(blog.getId());
+    }
+
+    private void queryUserByBlog(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
 }
