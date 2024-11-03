c654f7f61a65fd2c583016786f75194612fcf025
zhouqingkai <1208678644@qq.com>
2024-11-03 15:24:11 +0800

    feat(follow): 添加用户关注和查询是否关注的功能
    - 在 FollowController 中添加了 follow 和 isFollow 方法
    - 在 IFollowService 接口中定义了 follow 和 isFollow 方法
    - 在 FollowServiceImpl 中实现了 follow 和 isFollow 方法
    - 优化了代码结构，提高了可维护性

--- hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
@@ -1,13 +1,13 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author dadaguai
@@ -16,5 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+    private final IFollowService followService;
 
+    public FollowController(IFollowService followService) {
+        this.followService = followService;
+    }
+
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long id, @PathVariable("isFollow") Boolean isFollow) {
+        return followService.follow(id, isFollow);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long id) {
+        return followService.isFollow(id);
+    }
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,5 +1,8 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.constant.UserHolder;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
@@ -8,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author dadaguai
@@ -16,5 +19,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+    /**
+     * 关注/取关用户
+     *
+     * @param id
+     * @param isFollow
+     * @return
+     */
+    @Override
+    public Result follow(Long id, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        if (isFollow) {
+            Follow follow = new Follow().setFollowUserId(id).setUserId(userId);
+            this.save(follow);
+        } else {
+            //取关
+            this.remove(new QueryWrapper<Follow>().lambda().eq(Follow::getUserId, userId).eq(Follow::getFollowUserId, id));
+        }
+        return Result.ok();
+    }
 
+    //查询是否关注用户
+    @Override
+    public Result isFollow(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = this.query().eq("user_id", userId).eq("follow_user_id", id).count();
+        return Result.ok(count > 0);
+    }
 }
